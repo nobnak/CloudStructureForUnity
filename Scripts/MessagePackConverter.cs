eb54c9787645141063597655643861fe5dc8e075
Nakata Nobuyuki <nobu.nakata@gmail.com>
2021-04-29 18:45:28 +0900

    Init

---
+++ Scripts/MessagePackConverter.cs
@@ -0,0 +1,39 @@
+﻿using MessagePack;
+
+
+
+namespace CloudStructures.Converters
+{
+    /// <summary>
+    /// Provides value converter using MessagePack for C#.
+    /// </summary>
+    public sealed class MessagePackConverter : IValueConverter
+    {
+        public MessagePackConverter(bool compress = true) {
+            Options = MessagePackSerializerOptions.Standard.WithCompression(
+                compress ? MessagePackCompression.Lz4BlockArray : MessagePackCompression.None);
+        }
+
+        public MessagePackSerializerOptions Options { get; set; }
+
+        /// <summary>
+        /// Serialize value to binary.
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public byte[] Serialize<T>(T value)
+            => MessagePackSerializer.Serialize(value, Options);
+
+
+        /// <summary>
+        /// Deserialize value from binary.
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public T Deserialize<T>(byte[] value)
+            => MessagePackSerializer.Deserialize<T>(value, Options);
+    }
+}
+
